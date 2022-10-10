58c1285976ad5616a578c84ceff4f4a05c4582ff
neiko <neiko.damoi@etu.unistra.fr>
2022-10-10 16:27:51 +0200

    analyse et ajout de fcts

--- data/datacleaner.py
+++ data/datacleaner.py
@@ -13,6 +13,14 @@ def delete_ponctuation_of_caption(caption):
     
     return caption_no_punctuation
 
+def lower_caption(caption):
+    """
+    Lowers the whole caption
+    Args:
+        caption (string): the caption to lower
+    """
+    return caption.lower()
+
 def tokenize_caption(caption):
     """
     Caption tokenizer
@@ -25,13 +33,26 @@ def tokenize_caption(caption):
     tokens = word_tokenize(caption)
     return tokens
 
-def delete_stopwords(caption):
+def delete_stopwords(token_list):
+    """
+    Removes the stopwords from any tokenlist
+    Args:
+        token_list (list array): array containing the tokens
+
+    Returns:
+        list array: the same tokenlist without stopwords
+    """
     stopwords = nltk.corpus.stopwords.words('english')
 
-    output= [i for i in caption if i not in stopwords]
+    output= [i for i in token_list if i not in stopwords]
+    return output
+
+def remove_non_alphanumeric(caption):
+    output = ''.join([i for i in caption if i.isalnum()])
     return output
 
 def clean_captions(dataset, column_name_of_captions):
+    assert isinstance(column_name_of_captions, str)
     """
     Function removing punctuation, lowering text, removing stopwords and tokenizing the data.
     Args:
@@ -40,8 +61,9 @@ def clean_captions(dataset, column_name_of_captions):
     Returns:
         array = array of tokenized captions
     """
-    clean_msgs= dataset[column_name_of_captions].apply(lambda x:delete_ponctuation_of_caption(x)).copy()
-    clean_msgs_lowered = clean_msgs.apply(lambda x: x.lower()).copy()
+    clean_msgs= dataset[column_name_of_captions].apply(lambda x: delete_ponctuation_of_caption(x)).copy()
+    clean_msgs = clean_msgs.apply(lambda x: remove_non_alphanumeric(x))
+    clean_msgs_lowered = clean_msgs.apply(lambda x: lower_caption(x)).copy()
     clean_msgs_lowered_tokenized = clean_msgs_lowered.apply(lambda x: tokenize_caption(x)).copy()
     clean_msgs_lowered_tokenized_noStopwords = clean_msgs_lowered_tokenized.apply(lambda x: delete_stopwords(x))
     return clean_msgs_lowered_tokenized_noStopwords
--- data/test_datacleaner.py
+++ data/test_datacleaner.py
@@ -26,13 +26,67 @@ def test_tokenize_caption():
     result2 = datacleaner.tokenize_caption(test_caption2)
     result3 = datacleaner.tokenize_caption(test_caption3)
     result4 = datacleaner.tokenize_caption(test_caption4)
-    print(result1)
+    
     assert result1 == ['The', 'lonely', 'wolf', 'sits', 'in', 'the', 'yellow', 'house']
     assert result2 == ['We', 'the', 'guardians', 'of', 'the', 'galaxy', 'came', 'to', 'put', 'you', 'to', 'an', 'end']
     assert result3 == ['This', 'sentence', 'contains', 'no', 'punctuation']
     assert result4 == ['Sentence', 'without', 'stopwords', 'cool', 'thing', 'guess']
 
+def test_delete_stopwords():
+    test_caption1 = ['The', 'lonely', 'wolf', 'sits', 'in', 'the', 'yellow', 'house']
+    test_caption2 = ['We', 'the', 'guardians', 'of', 'the', 'galaxy', 'came', 'to', 'put', 'you', 'to', 'an', 'end']
+    test_caption3 = ['This', 'sentence', 'contains', 'no', 'punctuation']
+    test_caption4 = ['Sentence', 'without', 'stopwords', 'cool']
 
+    
+    result1 = datacleaner.delete_stopwords(test_caption1)
+    result2 = datacleaner.delete_stopwords(test_caption2)
+    result3 = datacleaner.delete_stopwords(test_caption3)
+    result4 = datacleaner.delete_stopwords(test_caption4)
+    
+    assert result1 == ['The', 'lonely', 'wolf', 'sits', 'yellow', 'house']
+    assert result2 == ['guardians', 'galaxy', 'came', 'put', 'end']
+    assert result3 == ['This', 'sentence', 'contains', 'punctuation']
+    assert result4 == ['Sentence', 'without', 'stopwords', 'cool']
+    
+    # the stopwords remover seems to be faulty, we will have to check that later
+
+def test_remove_non_alphanumeric():
+    test_caption1 = "The lonely @wolf, sits in the yellow house." 
+    test_caption2 = "We, the guardians of +the galaxy, came to put you to an end !"
+    test_caption3 = "This se|ntence #contains no pu|nctuation"
+    test_caption4 = "Sentence without stopwords cool thing guess"
+    
+    result1 = datacleaner.remove_non_alphanumeric(test_caption1)
+    result2 = datacleaner.remove_non_alphanumeric(test_caption2)
+    result3 = datacleaner.remove_non_alphanumeric(test_caption3)
+    result4 = datacleaner.remove_non_alphanumeric(test_caption4)
+
+    assert result1 == "The lonely wolf, sits in the yellow house." 
+    assert result2 == "We, the guardians of the galaxy, came to put you to an end !"
+    assert result3 == "This sentence contains no punctuation"
+    assert result4 == "Sentence without stopwords cool thing guess"
+    
+
+def test_lower_caption():
+    test_caption1 = "The lonely Wolf, sits in The yellow house." 
+    test_caption2 = "We, the guardians of The galaxy, came to put you TO an end !"
+    test_caption3 = "this sentence contains no punctuation"
+    test_caption4 = "SenTenCe without stopwords, cool thing guess"
+    
+    result1 = datacleaner.lower_caption(test_caption1)
+    result2 = datacleaner.lower_caption(test_caption2)
+    result3 = datacleaner.lower_caption(test_caption3)
+    result4 = datacleaner.lower_caption(test_caption4)
+
+    assert result1 == "the lonely wolf, sits in the yellow house."
+    assert result2 == "we, the guardians of the galaxy, came to put you to an end !"
+    assert result3 == "this sentence contains no punctuation"
+    assert result4 == "sentence without stopwords, cool thing guess"
+       
 if __name__ == '__main__':
     test_delete_ponctuation_of_caption()    
     test_tokenize_caption()
+    test_delete_stopwords()
+    test_lower_caption()
+    test_remove_non_alphanumeric()
