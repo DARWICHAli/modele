d9d5e482d1b1098bada5f535cac14d3d7f4c1917
neiko <neiko.damoi@etu.unistra.fr>
2022-12-28 15:59:26 +0100

    segmentation

--- data/encoderCNN_pretrained.py
+++ data/encoderCNN_pretrained.py
@@ -1,38 +1,54 @@
 from torchvision import models
 from torch import nn
+from PIL import Image
+import torchvision.transforms as T
+import cv2 
+import matplotlib.pyplot as plt
 
 class EncoderCNN(nn.Module):
-    """Encoder inputs images and returns feature maps.
-    Aruments:
-    ---------
-    - image - augmented image sample
-    
-    Returns:
-    ---------
-    - features - feature maps of size (batch, height*width, #feature maps)
+    """Encoder inputs images and returns feature boxes.
     """
+    
+    
     def __init__(self):
         super(EncoderCNN, self).__init__()
-        resnet = models.resnet152(pretrained=True)
-        for param in resnet.parameters():
-            param.requires_grad_(False)
+        self.model = models.detection.fasterrcnn_resnet50_fpn(pretrained=True) 
         
-        modules = list(resnet.children())[:-2]
-        self.resnet = nn.Sequential(*modules)
+        self.model.eval()
         
-    def forward(self, images):
-        features = self.resnet(images)
-        # first, we need to resize the tensor to be 
-        # (batch, size*size, feature_maps)
-        batch, feature_maps, size_1, size_2 = features.size()       
-        features = features.permute(0, 2, 3, 1)
-        features = features.view(batch, size_1*size_2, feature_maps)
-       
-        return features
-    
-    def fine_tune(self, fine_tune=True):
-        for p in self.resnet.parameters():
-            p.requires_grad = False
-        for c in list(self.resnet.children())[5:]:
-            for p in c.parameters():
-                p.requires_grad = fine_tune
+        self.coco_instances = ['__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light', 
+                          'fire hydrant', 'N/A', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 
+                          'bear', 'zebra', 'giraffe', 'N/A', 'backpack', 'umbrella', 'N/A', 'N/A', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 
+                          'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket', 'bottle', 
+                          'N/A', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 
+                          'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'N/A', 'dining table', 'N/A', 'N/A', 'toilet', 
+                          'N/A', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 
+                          'N/A', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush']
+
+                
+    def get_prediction(self, img_path, threshold):
+        """
+        get_prediction
+            parameters:
+            - img_path - path of the input image
+            - threshold - threshold value for prediction score
+            method:
+            - Image is obtained from the image path
+            - the image is converted to image tensor using PyTorch's Transforms
+            - image is passed through the model to get the predictions
+            - class, box coordinates are obtained, but only prediction score > threshold
+                are chosen.
+        """
+        img = Image.open(img_path)
+        transform = T.Compose([T.ToTensor()])
+        img = transform(img)
+        pred = self.model([img])
+        pred_class = [self.coco_instances[i] for i in list(pred[0]['labels'].numpy())]
+        pred_boxes = [[(i[0], i[1]), (i[2], i[3])] for i in list(pred[0]['boxes'].detach().numpy())]
+        pred_score = list(pred[0]['scores'].detach().numpy())
+        pred_t = [pred_score.index(x) for x in pred_score if x>threshold][-1]
+        pred_boxes = pred_boxes[:pred_t+1]
+        pred_class = pred_class[:pred_t+1]
+        return  img, pred_boxes, pred_class
+
+   
